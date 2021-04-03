de0528c8f959179d12ca7797356757d4656fc704
vlyashko02 <vladimir.lyashko02@gmail.com>
2021-04-03 15:52:26 +0300

    Tests for Delete

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Metadata;
 using System;
 using CommandAPI.Data;
 using Xunit;
@@ -204,6 +205,36 @@ namespace CommandAPI.Tests
             var controller = new CommandsController(mockRepo.Object, mapper);
 
             var result = controller.PartialCommandUpdate(0, new Microsoft.AspNetCore.JsonPatch.JsonPatchDocument<CommandUpdateDto> {  });
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+        [Fact]
+        public void DeleteCommand_Returns204NoContent_WhenValidResourceIDSubmitted()
+        {
+            mockRepo.Setup(repo =>
+                repo.GetCommandById(1)).Returns(
+                    new Command 
+                    { 
+                        Id = 1,
+                        HowTo = "mock",
+                        Platform = "Mock",
+                        CommandLine = "Mock" 
+                    });
+            
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            var result = controller.DeleteCommand(1);
+
+            Assert.IsType<NoContentResult>(result);
+        }
+        [Fact]
+        public void DeleteCommand_Returns404NotFound_WhenNonExistentResourseIDSubmitted()
+        {
+            mockRepo.Setup(repo => repo.GetCommandById(0)).Returns(() => null);
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            var result = controller.DeleteCommand(1);
 
             Assert.IsType<NotFoundResult>(result);
         }
